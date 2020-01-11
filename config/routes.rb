3c607ee2fc5336db2e46e92609e93de752aa3500
Seva Rybakov <jumpincode@gmail.com>
2020-01-11 12:26:08 +0300

    Add products index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :products
   resources :product_groups
   resources :categories
-  root 'home#index'
+  root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
