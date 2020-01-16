d0d57f3123ea3f4832216a38ee73ee0c24c0db4b
Seva Rybakov <jumpincode@gmail.com>
2020-01-16 14:07:46 +0300

    Add cart view

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   resources :categories do
     resources :product_groups 
   end
+
+  get '/cart' => 'cart#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
