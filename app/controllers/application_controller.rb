53f73b546ffef0f91279b376ab22b64d656f060d
AliAlsayed <a_alsayed87@hotmail.com>
2020-01-18 22:46:20 +0400

    add add_to_cart method and connect view to it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  def current_cart
+    session_cart ||= {}
+  end
+
+  def add_to_cart
+    fail
+    if current_cart[params[:product_id]]
+      current_cart[params[:product_id]] += params[:quantity].to_i
+    else
+      current_cart[params[:product_id]] = params[:quantity].to_i
+    end
+    redirect_back fallback_location: root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     resources :product_groups 
   end
 
+  post '/cart' => 'application#add_to_cart'
   get '/cart' => 'cart#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
